64fa794693549e67be4a3a1bc4aceb9728b31fc6
Samuel Kerr <transiencymail@gmail.com>
2021-02-24 07:40:12 -0700

    navbar colorchange

---
+++ www/app/javascript/packs/application.js
@@ -0,0 +1,25 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+require('jquery')
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= bootstrap-sprockets
+//= require tree .
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
+/*$(document).ready(function() {
+	alert('foobar');
+});
+*/
